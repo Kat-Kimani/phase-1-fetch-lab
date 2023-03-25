437e3c531e68eb983a4bc667dd95db1945dc55b6
catherine <wachukimani@gmail.com>
2023-03-25 12:32:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+ fetch("https://anapioficeandfire.com/api/books")
+ .then((resp) => resp.json())
+.then(books =>  (renderBooks(books), books))
+
+ 
+
+
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
